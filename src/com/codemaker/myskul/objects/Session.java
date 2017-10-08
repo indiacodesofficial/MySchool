dcdf8deec21cf9fe9bc51ff11415fee87c17a9b0
iamrajatroy <rajatr62@gmail.com>
2017-10-08 14:30:34 +0530

    Added bootstrap to view and added session object

---
+++ src/com/codemaker/myskul/objects/Session.java
@@ -0,0 +1,27 @@
+package com.codemaker.myskul.objects;
+
+public class Session {
+
+	private String userName;
+	
+	private String userType;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+	
+	
+	
+}
